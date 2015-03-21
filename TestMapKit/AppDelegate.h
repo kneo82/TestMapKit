5e8e56b1bb15c6c5612d7ef336dc59160232a7c7
Admin <kneo82@gmail.com>
2015-03-21 13:01:43 +0300

    Initial Commit

---
+++ TestMapKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestMapKit
+//
+//  Created by Admin on 21/03/15.
+//  Copyright (c) 2015 IDAPGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestMapKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestMapKit
+//
+//  Created by Admin on 21/03/15.
+//  Copyright (c) 2015 IDAPGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
